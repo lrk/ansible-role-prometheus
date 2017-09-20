15b778b8b70f372561deb6f4002a56208d46e783
Lrk <cengis.kocaurlu@gmail.com>
2017-09-20 18:59:02 +0200

    fix download indepotency problem

--- run_test_vagrant.sh
+++ run_test_vagrant.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set +x
 
-if [ ! -d "/vagrant/tests/roles "]
+if [ ! -d "/vagrant/tests/roles" ]
 then
   mkdir -p /vagrant/tests/roles
 fi
 
-if [ ! -d "/vagrant/tests/roles/ansible.prometheus"]
+if [ ! -d "/vagrant/tests/roles/ansible.prometheus" ]
 then
-  ln -s /vagrant/tests/roles/ansible-prometheus /vagrant
+  ln -s /vagrant /vagrant/tests/roles/ansible-prometheus
 fi
 
-ansible-playbook -i tests/inventory --connection=local tests/vagranttest.yml
+ansible-playbook -i tests/inventory --connection=local tests/vagranttest.yml $*
